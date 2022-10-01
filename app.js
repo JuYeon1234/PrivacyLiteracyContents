fb122be12fbe98ab55fd008406cdc73ff4a6466b
JuYeon1234 <45138436+JuYeon1234@users.noreply.github.com>
2022-10-01 23:25:15 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,98 @@
+const express = require('express');
+const mysql = require("mysql2/promise");
+// const mysql = require("mysql2/promise");
+const app = express();
+const port = 8080;
+
+
+
+
+// * ===== html 렌더링 ===== * //
+app.engine('html', require('ejs').renderFile)
+
+app.set('view engine', 'html')
+app.get('/',(req, res)=>{
+    res.send('hello world')
+});
+
+app.get('/index',(req, res)=>{
+    //res.render('C:\Users\juyeo\Desktop\2022\project\jy\index.html')
+    res.render('index')
+});
+
+app.get('/insert', async (req, res) => {
+
+    // * ===== DB 연결 ===== * //
+    const mysql = require('mysql2/promise');
+    const connection = await mysql.createConnection({
+            host: '34.64.161.186',
+            user: 'root',
+            password: '1234',
+            database: 'juyeon'
+        });
+
+
+    try{
+        await connection.connect();
+    } catch(error){
+        console.log(error)
+    }
+
+    const teacher_Id = req.query.teacher_Id;
+    const class_no = req.query.class_no;
+    let pw = req.query.pw;
+    try {
+        await connection.query(`insert into teacher(teacher_Id, class_no, pw) values('${teacher_Id}',${class_no}, '${pw}')`)
+        res.send('true')
+    }catch(error){
+        res.send('false')
+    }
+})
+
+app.get('/createclass',(req, res)=>{
+    res.render('createclass')
+});
+
+const server = app.listen(port,()=> {
+    console.log('서버가 실행되었습니다.');
+})
+
+app.get('/receive_python', (req, res) => {
+    const test1 = req.query.data;
+    console.log(test1);
+    res.send("받았습니다.");
+})
+
+
+
+// * ===== socket 통신 ===== * //
+
+
+app.get('/receive_python', async (req, res) => {
+
+    // * ===== DB 연결 ===== * //
+    const mysql = require('mysql2/promise');
+    const connection = await mysql.createConnection({
+            host: '34.64.161.186',
+            user: 'root',
+            password: '1234',
+            database: 'juyeon'
+        });
+
+        
+    try{
+        await connection.connect();
+    } catch(error){
+        console.log(error)
+    }
+
+    const test1 = req.query.data;
+    console.log(test1);
+    res.send("받았습니다.");
+    try {
+        await connection.query(`insert into teacher(teacher_Id, class_no, pw) values('${teacher_Id}',${class_no}, '${pw}')`)
+        res.send('true')
+    }catch(error){
+        res.send('false')
+    }
+})
