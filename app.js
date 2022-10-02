7761d1bef3b5a8615d3a3563bb3cc7f24c7acc11
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2022-10-02 19:19:14 +0900

    프로세스 및 서버 종료 시 db 연결 해제

--- app.js
+++ app.js
@@ -1,16 +1,32 @@
+// * ===== 사용할 패키지 호출 및 mysql 셋팅 ===== * //
 const express = require('express');
-const mysql = require("mysql2/promise");
-// const mysql = require("mysql2/promise");
 const app = express();
 const port = 8080;
+const mysql = require('mysql2/promise');
+let connection;
+const mysqlConnectionSet = (async()=>{
+    try{
+        // db config set
+        connection = await mysql.createConnection({
+            host: '34.64.161.186',
+            user: 'root',
+            password: '1234',
+            database: 'juyeon'
+        });
 
+        // db connect
+        await connection.connect();
 
-
+    } catch(error){
+        console.log(error)
+    }
+})()
 
 // * ===== html 렌더링 ===== * //
 app.engine('html', require('ejs').renderFile)
-
 app.set('view engine', 'html')
+
+// * ===== url router ===== * //
 app.get('/',(req, res)=>{
     res.send('hello world')
 });
@@ -21,28 +37,11 @@ app.get('/index',(req, res)=>{
 });
 
 app.get('/insert', async (req, res) => {
-
-    // * ===== DB 연결 ===== * //
-    const mysql = require('mysql2/promise');
-    const connection = await mysql.createConnection({
-            host: '34.64.161.186',
-            user: 'root',
-            password: '1234',
-            database: 'juyeon'
-        });
-
-
-    try{
-        await connection.connect();
-    } catch(error){
-        console.log(error)
-    }
-
     const teacher_Id = req.query.teacher_Id;
     const class_no = req.query.class_no;
     let pw = req.query.pw;
     try {
-        await connection.query(`insert into teacher(teacher_Id, class_no, pw) values('${teacher_Id}',${class_no}, '${pw}')`)
+       await connection.query(`insert into teacher(teacher_Id, class_no, pw) values('${teacher_Id}',${class_no}, '${pw}')`)
         res.send('true')
     }catch(error){
         res.send('false')
@@ -53,39 +52,40 @@ app.get('/createclass',(req, res)=>{
     res.render('createclass')
 });
 
+
+app.get('/receive_python', async (req, res) => {
+
+    const data = req.query.data;
+    console.log(data);
+    const datas = data.split(',');
+    console.log(datas);
+    try {
+        await connection.query(`insert into student(teacher_Id, std_no, day1_pt, day2_pt, day3_pt, day4_pt, day5_pt, total, security_point) values('${datas[0]}','${datas[1]}', '${datas[2]}','${datas[3]}','${datas[4]}','${datas[5]}', '${datas[6]}','${datas[7]}', ${datas[8]})`)
+        res.send("받았습니다.");
+    } catch(error){
+        res.send('실패했습니다.')
+    }
+})
+
+// * ===== app listen ===== * //
 const server = app.listen(port,()=> {
     console.log('서버가 실행되었습니다.');
 })
 
+// * ===== app 종료  ===== * //
+const exitFunction = async () => {
+    await connection.end();
+    server.close()
+    process.exit()
+}
 
-
-
-// * ===== socket 통신 ===== * //
-
-
-app.get('/receive_python', async (req, res) => {
-
-    const mysql = require('mysql2/promise');
-    const connection = await mysql.createConnection({
-            host: '34.64.161.186',
-            user: 'root',
-            password: '1234',
-            database: 'juyeon'
-        });
-
-
-    try{
-        await connection.connect();
-    } catch(error){
-        console.log(error)
-    }
-
-    const data = req.query.data;
-    console.log(data);
-    datas = data.split(',');
-    console.log(datas);
-    //await connection.query(`insert into student(teacher_Id, std_no, day1_pt, day2_pt, day3_pt, day4_pt, day5_pt, total, security_point) values('${datas[0]}', '${datas[1]}','${datas[2]}','${datas[3]}','${datas[4]}','${datas[5]}','${datas[6]}','${datas[7]}','${datas[9]}', ${datas[1]})')
-    await connection.query(`insert into student(teacher_Id, std_no, day1_pt, day2_pt, day3_pt, day4_pt, day5_pt, total, security_point) values('${datas[0]}','${datas[1]}', '${datas[2]}','${datas[3]}','${datas[4]}','${datas[5]}', '${datas[6]}','${datas[7]}', ${datas[8]})`)
-    res.send("받았습니다.");
-   
+//do something when app is closing
+process.on('exit', async()=>{
+    console.log('process.close')
+    await exitFunction()
+})
+//catches ctrl+c event
+process.on('SIGINT', async()=>{
+    console.log('process.SIGINT');
+    await exitFunction()
 })
