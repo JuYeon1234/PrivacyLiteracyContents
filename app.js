8818b4c8e193b20937da6cfd075aed54963d87ba
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2022-10-04 21:51:41 +0900

    ㅇㅔ러 발생할 경우 재시작

--- app.js
+++ app.js
@@ -5,75 +5,76 @@ const port = 8080;
 const mysql = require('mysql2/promise');
 let connection;
 
-app.use(express.static("views"));
-const mysqlConnectionSet = (async()=>{
-    try{
-        // db config set
-        connection = await mysql.createConnection({
-            host: '34.64.161.186',
-            user: 'root',
-            password: '1234',
-            database: 'juyeon'
-        });
+function appSet(){
+    app.use(express.static("views"));
+    const mysqlConnectionSet = (async()=>{
+        try{
+            // db config set
+            connection = await mysql.createConnection({
+                host: '34.64.161.186',
+                user: 'root',
+                password: '1234',
+                database: 'juyeon'
+            });
 
-        // db connect
-        await connection.connect();
+            // db connect
+            await connection.connect();
 
-    } catch(error){
-        console.log(error)
-    }
-})()
+        } catch(error){
+            console.log(error)
+        }
+    })()
 
-// * ===== html 렌더링 ===== * //
-app.engine('html', require('ejs').renderFile)
-app.set('view engine', 'html')
+    // * ===== html 렌더링 ===== * //
+    app.engine('html', require('ejs').renderFile)
+    app.set('view engine', 'html')
 
-// * ===== url router ===== * //
-app.get('/',(req, res)=>{
-    res.send('hello world')
-});
+    // * ===== url router ===== * //
+    app.get('/',(req, res)=>{
+        res.send('hello world')
+    });
 
-app.get('/index',(req, res)=>{
-    //res.render('C:\Users\juyeo\Desktop\2022\project\jy\index.html')
-    res.render('index')
-});
+    app.get('/index',(req, res)=>{
+        //res.render('C:\Users\juyeo\Desktop\2022\project\jy\index.html')
+        res.render('index')
+    });
 
-app.get('/insert', async (req, res) => {
-    const teacher_Id = req.query.teacher_Id;
-    const class_no = req.query.class_no;
-    let pw = req.query.pw;
-    try {
-        await connection.query(`insert into teacher(teacher_Id, class_no, pw) values('${teacher_Id}',${class_no}, '${pw}')`)
-        res.send('계정이 생성되었습니다.')
-    }catch(error){
-        res.send('정보가 잘못되었습니다. 다른 정보를 입력해주세요.')
-    }
-})
+    app.get('/insert', async (req, res) => {
+        const teacher_Id = req.query.teacher_Id;
+        const class_no = req.query.class_no;
+        let pw = req.query.pw;
+        try {
+            await connection.query(`insert into teacher(teacher_Id, class_no, pw) values('${teacher_Id}',${class_no}, '${pw}')`)
+            res.send('계정이 생성되었습니다.')
+        }catch(error){
+            res.send('정보가 잘못되었습니다. 다른 정보를 입력해주세요.')
+        }
+    })
 
-app.get('/createclass',(req, res)=>{
-    res.render('createclass')
-});
+    app.get('/createclass',(req, res)=>{
+        res.render('createclass')
+    });
 
 
-app.get('/receive_python', async (req, res) => {
+    app.get('/receive_python', async (req, res) => {
 
-    const data = req.query.data;
-    console.log(data);
-    const datas = data.split(',');
-    console.log(datas);
-    try {
-        await connection.query(`insert into student(teacher_Id, std_no, day1_pt, day2_pt, day3_pt, day4_pt, day5_pt, total, security_point) values('${datas[0]}','${datas[1]}', '${datas[2]}','${datas[3]}','${datas[4]}','${datas[5]}', '${datas[6]}','${datas[7]}', ${datas[8]})`)
-        res.send("받았습니다.");
-    } catch(error){
-        res.send('실패했습니다.')
-    }
-})
+        const data = req.query.data;
+        console.log(data);
+        const datas = data.split(',');
+        console.log(datas);
+        try {
+            await connection.query(`insert into student(teacher_Id, std_no, day1_pt, day2_pt, day3_pt, day4_pt, day5_pt, total, security_point) values('${datas[0]}','${datas[1]}', '${datas[2]}','${datas[3]}','${datas[4]}','${datas[5]}', '${datas[6]}','${datas[7]}', ${datas[8]})`)
+            res.send("받았습니다.");
+        } catch(error){
+            res.send('실패했습니다.')
+        }
+    })
 
-app.get('/student_point_table',(req, res)=>{
-    res.render('student_point_table')
-});
+    app.get('/student_point_table',(req, res)=>{
+        res.render('student_point_table')
+    });
 
-app.get('/select_student',async(req, res)=>{
+    app.get('/select_student',async(req, res)=>{
         const chk_id = req.query.t_id;
         const chk_pw = req.query.pw;
         //r = await connection.query('select * from student');
@@ -100,27 +101,35 @@ app.get('/select_student',async(req, res)=>{
         } catch(error){
             res.send('false')
         }
-});
+    });
 
-// * ===== app listen ===== * //
-const server = app.listen(port,()=> {
-    console.log('서버가 실행되었습니다.');
-})
+    // * ===== app listen ===== * //
+    const server = app.listen(port,()=> {
+        console.log('서버가 실행되었습니다.');
+    })
 
-// * ===== app 종료  ===== * //
-const exitFunction = async () => {
-    await connection.end();
-    server.close()
-    process.exit()
+    // * ===== app 종료  ===== * //
+    const exitFunction = async () => {
+        await connection.end();
+        server.close()
+        process.exit()
+    }
+
+    //do something when app is closing
+    process.on('exit', async()=>{
+        console.log('process.close')
+        await exitFunction()
+    })
+    //catches ctrl+c event
+    process.on('SIGINT', async()=>{
+        console.log('process.SIGINT');
+        await exitFunction()
+    })
 }
 
-//do something when app is closing
-process.on('exit', async()=>{
-    console.log('process.close')
-    await exitFunction()
-})
-//catches ctrl+c event
-process.on('SIGINT', async()=>{
-    console.log('process.SIGINT');
-    await exitFunction()
-})
+try {
+    appSet();
+}catch(e){
+    console.error(e);
+    appSet();
+}
